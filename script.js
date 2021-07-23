7d00d74f3d0d67a1d8667ae0739f476818f14be6
bphennessy <87782392+bphennessy@users.noreply.github.com>
2021-07-22 22:06:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ d3.csv("https://raw.githubusercontent.com/bphennessy/CS416NarrativeVisualization
     svg.append("path")
         .datum(ByYear)
         .attr("class", "line")
-        .attr('d', valueline)
+        .attr("d", valueline)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
 
@@ -48,32 +48,4 @@ d3.csv("https://raw.githubusercontent.com/bphennessy/CS416NarrativeVisualization
 
     svg.append("g")
         .call(d3.axisLeft(y));
-    /*d3.select("svg").append("g").attr("transform","translate(50,50)")
-        .selectAll("circle").data(ByYear).enter().append("circle")
-            .attr("cx",2015)//function(d) { return d.key; })
-            .attr("cy",function(d) { return d.values.averageFE })
-            .attr("r",5);
-            //.attr("r",function(d) { return +d.EngineCylinders+2 });
-    /*xs = d3.scaleLog().base(10).domain([10,150]).range([0,200])
-    ys = d3.scaleLog().base(10).domain([10,150]).range([200,0])
-
-    d3.select("svg").append("g").attr("transform","translate(50,50)")
-        .selectAll("circle").data(ga2022).enter().append("circle")
-            .attr("cx",function(d,i) { return xs(d.AverageCityMPG) })
-            .attr("cy",function(d) { return ys(d.AverageHighwayMPG) })
-            .attr("r",function(d) { return +d.EngineCylinders+2 });*/
-
-   /* var x = d3.scaleLinear().domain([2010,2025]).range([0,200]);
-    var y = d3.scaleLinear().domain([0,30]).range([200,0]);
-    var xticks = [2010,2015,2020,2025];
-    var yticks = [0,10,20,30];
-
-    d3.select("svg").append("g").attr("transform","translate(50,50)")
-        .call(d3.axisLeft(y)
-        .tickValues(yticks)
-        .tickFormat(d3.format("~s")));
-    d3.select("svg").append("g").attr("transform","translate(50,250)")
-        .call(d3.axisBottom(x)
-        .tickValues(xticks)
-        .tickFormat(d3.format("~")));*/
 });
